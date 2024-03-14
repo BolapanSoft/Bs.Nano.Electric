c1d9d84dd84346e3bc4be1a46f97a80ab51e2cda
Mishkin I.N <Mishkin_Ivan@mail.ru>
2024-03-14 17:48:30 +0400

    Bs.Nano.Electric.Report v1.0.0

---
+++ Model/Enums/ScsGutterPassageType.cs
@@ -0,0 +1,11 @@
+﻿using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
+namespace Nano.Electric.Enums {
+    public enum ScsGutterPassageType {
+        [Description("Центральный")]
+        MIDDLE = 0,
+        [Description("Правый")]
+        RIGHT = 1,
+        [Description("Левый")]
+        LEFT = 2
+    }
+}
