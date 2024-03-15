465c04b36f25cffa2181b04a88a3801a7b27338a
Mishkin I.N <Mishkin_Ivan@mail.ru>
2024-03-15 16:31:45 +0400

    Nano.Electric.Model v 1.0.32

---
+++ Model/DefaultLocalizeValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nano.Electric {
+    [AttributeUsage(AttributeTargets.Field|AttributeTargets.Property|AttributeTargets.Class, AllowMultiple = false)]
+    public class DefaultLocalizeValueAttribute : Attribute {
+        public string DefaultLocalizeValue { get; }
+        public DefaultLocalizeValueAttribute(string defaultLocalizeValue) {
+            DefaultLocalizeValue = defaultLocalizeValue;
+        }
+    }
+}
