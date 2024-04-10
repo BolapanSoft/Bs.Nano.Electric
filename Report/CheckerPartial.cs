2ea8bc591044b5fbb23246199bb435fb1bf1d1d3
Mishkin I.N <Mishkin_Ivan@mail.ru>
2024-04-10 08:54:27 +0400

    Bs.Nano.Electric.Model v1.0.32 add Bs.Nano.Electric.Report v1.0.0

---
+++ Report/CheckerPartial.cs
@@ -0,0 +1,51 @@
+﻿using Nano.Electric;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Bs.Nano.Electric.Report
+{
+    public partial class Checker {
+        private struct Product : IProduct {
+            public string Code { get; set; }
+
+            public int id { get; set; }
+            public int? DbImageRef { get; set; }
+            public string Name { get; set; }
+            public string Manufacturer { get; set; }
+        }
+        /// <summary>
+        /// Возвращает запись индекса для имени переменной.
+        /// </summary>
+        /// <param name="ruleName"></param>
+        /// <returns></returns>
+        /// <remarks>Имя переменной должно соответствовать шаблону "\w+(_\n+)+".</remarks>
+        private static string GetIndex(string? ruleName) {
+            if (string.IsNullOrEmpty(ruleName))
+                return string.Empty;
+            return ruleName!.Replace('_', '.');
+        }
+        public static string[] SplitToWords(string value) {
+            // Split the string into words based on letters and digits from any language
+            var matches = Regex.Matches(value, @"(?>[\p{L}\p{N}]+([,.]\p{N}+)*)");
+            List<string> words = new List<string>(64);
+            foreach (Match m in matches) {
+                words.Add(m.Value);
+            }
+            return words.ToArray();
+        }
+        public static bool IsEnglishWord(string word) {
+            // Match the word against the pattern that allows only English alphabets and digits
+            bool isMatch = Regex.IsMatch(word, @"^[A-Za-z0-9]+$");
+            return isMatch;
+        }
+        public static bool IsRussianWord(string word) {
+            // Match the word against the pattern that allows only Russian alphabets and digits
+            return Regex.IsMatch(word, @"^[ А-Яа-я0-9]+$");
+        }
+        public static bool IsRepresentNumber(string word) {
+            // Match the word that represent a number 
+            return Regex.IsMatch(word, @"\p{N}+([,.]\p{N}+)*");
+        }
+
+    }
+}
---
+++ Report/Resources.cs
@@ -0,0 +1,49 @@
+﻿using Nano.Electric;
+using System.Collections.Generic;
+
+namespace Bs.Nano.Electric.Report {
+    internal class Resources {
+        public const string DbCaeMaterialUtilitySet_Description = "Материалы и комплектация\\Комплектации материалов";
+        public const string DbGcMountSystemSet_Description = "Конфигурации КНС\\Конфигурации трасс лотков";
+        public const string DbScsGutterUtilitySet_Description = "Крепления лотков\\Конфигурации узлов крепления";
+        public const string DbScsGcSeriaConfiguration_Description = "Лотки\\Конфигурации соединительных элементов";
+        /*
+- КНС.Крепления лотков#Аксессуары крепления 20230601.xlsx
+- КНС.Крепления лотков#Элементы крепления 20230601.xlsx
+- КНС.Лотки#Аксессуары лотков 20230601.xlsx
+- КНС.Лотки#Перегородки 20230601.xlsx
+- КНС.Лотки.Секции прямые#Крышки 20230601.xlsx
+- КНС.Лотки.Секции прямые#Лотки 20230601.xlsx
+- КНС.Лотки.Секции соединительные#Крышки 20230601.xlsx
+- КНС.Лотки.Секции соединительные#Лотки 20230601.xlsx
+- КНС.Материалы и комплектация#Материалы 20230605.xlsx
+ */
+
+        //private static readonly Dictionary<string, string> tableDescriptions = new Dictionary<string, string> {
+        //        { nameof(ScsGutterCanal), "КНС.Лотки.Секции прямые#Лотки 20230601.xlsx" },
+        //        { nameof(DbScsGutterCover), "КНС.Лотки.Секции прямые#Крышки 20230601.xlsx" },
+        //        { nameof(DbScsGutterPartition), "КНС.Лотки#Перегородки 20230601.xlsx" },
+        //        { nameof(ScsGcFitting), "КНС.Лотки.Секции соединительные#Лотки 20230601.xlsx" },
+        //        { nameof(DbScsGcCoverUnit), "КНС.Лотки.Секции соединительные#Крышки 20230601.xlsx" },
+        //        { nameof(DbScsGcAccessoryUnit), "КНС.Лотки#Аксессуары лотков 20230601.xlsx" },
+        //        { nameof(ScsGutterBolting), "КНС.Крепления лотков#Элементы крепления 20230601.xlsx" },
+        //        { nameof(DbScsGcBoltingAccessoryUnit), "КНС.Крепления лотков#Аксессуары крепления 20230601.xlsx" },
+        //        { nameof(CaeMaterialUtility), "КНС.Материалы и комплектация#Материалы 20230605.xlsx" }
+        //    };
+        private static readonly Dictionary<string, string> tableImageCategories = new Dictionary<string, string> {
+                { nameof(ScsGutterCanal), "Секции прямые\\Лотки" },
+                { nameof(DbScsGutterCover), "Секции прямые\\Крышки" },
+                { nameof(DbScsGutterPartition), "Секции прямые\\Перегородки" },
+                { nameof(ScsGcFitting), "Секции соединительные\\Лотки" },
+                { nameof(DbScsGcCoverUnit), "Секции соединительные\\Крышки" },
+                { nameof(DbScsGcAccessoryUnit), "Аксессуары лотков" },
+                { nameof(ScsGutterBolting), "Крепления лотков\\Элементы крепления" },
+                { nameof(DbScsGcBoltingAccessoryUnit), "Крепления лотков\\Аксессуары крепления" },
+                { nameof(CaeMaterialUtility), "Материалы и комплектация\\Материалы" },
+                { nameof(DbScsGutterUtilitySet), "Крепления лотков\\Конфигурации узлов крепления" },
+                { nameof(DbGcMountSystem), "Конфигурации КНС\\Конфигурации трасс лотков" },
+
+           };
+        public static IReadOnlyDictionary<string, string> ImageCategory { get { return tableImageCategories; } }
+    }
+}
