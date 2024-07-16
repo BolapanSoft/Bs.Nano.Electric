414bf36c9e64e9a1c984ab47f643289023eb365b
Mishkin I.N <Mishkin_Ivan@mail.ru>
2024-07-16 23:41:22 +0400

    Add comments to KitStructureType.cs.

--- Model/Enums/KitStructureType.cs
+++ Model/Enums/KitStructureType.cs
@@ -20,13 +20,26 @@ DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
 
 namespace Nano.Electric.Enums {
     public enum KitStructureType {
+        [Description("Не определено")]
         Undefined=0,
+        /// <summary>
+        /// Лоток
+        /// </summary>
         [Description("Лоток")]
         Gutter=1,
+        /// <summary>
+        /// Полка
+        /// </summary>
         [Description("Полка")]
         DbShelf = 2,
+        /// <summary>
+        /// Крепление ярусов
+        /// </summary>
         [Description("Крепление ярусов")]
         Bolting = 3,
+        /// <summary>
+        /// Комплектующие
+        /// </summary>
         [Description("Комплектующие")]
         DbUtilityUnit = 7,
     }
