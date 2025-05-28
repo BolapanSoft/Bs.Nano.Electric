77bde41fff7ec8d70c0095277321002d3521b466
Mishkin I.N. <Mishkin_Ivan@mail.ru>
2025-05-28 12:53:43 +0400

    Add future report-database-content

--- Model/Partial/Context.cs
+++ Model/Partial/Context.cs
@@ -30,7 +30,149 @@ namespace Nano.Electric {
 
         private static Dictionary<string, string[]> propertiesCache = new Dictionary<string, string[]>();
         private static readonly Dictionary<Type, string> knownLocalizeValues = new Dictionary<Type, string>();
+        /// <summary>
+        /// Выполняет заполнение свойств сущности из сериализованного в строку источника.
+        /// </summary>
+        /// <typeparam name="Tdest">Тип сущности</typeparam>
+        /// <param name="product">Заполняемый экземпляр.</param>
+        /// <param name="propNames">Перечень наименований свойств для заполнения.</param>
+        /// <param name="item">Источник сериализованных в строку значений свойств.</param>
+        /// <returns>Перечень обработанных свойств и связанных исключений.</returns>
+        /// <remarks>Если свойство не удалось заполнить - возвращается имя свойства и возникшее при обработке исключение. 
+        /// Процедура обрабатывает только доступные для записи свойства. 
+        /// Если свойство является частью ключа сущности, то оно не может быть заполнено.</remarks>
+        public IList<(string, Exception)> FillProperties<Tdest>(Tdest product, IEnumerable<string> propNames, IReadOnlyDictionary<string, string> item, bool skipIfEmptyValue = true) where Tdest : class {
+            var results = new List<(string, Exception)>();
+            foreach (var propName in propNames) {
+                var propertyInfo = typeof(Tdest).GetProperty(propName);
+                if (propertyInfo == null || !propertyInfo.CanWrite) {
+                    continue;
+                }
 
+                if (propertyInfo.GetCustomAttribute<KeyAttribute>() != null) {
+                    results.Add((propName, new InvalidOperationException($"Операция не разрешена. Свойство {propName} является частью ключа.")));
+
+                    continue;
+                }
+                string sourceValue = item[propName];
+                if (skipIfEmptyValue && string.IsNullOrEmpty(sourceValue)) {
+                    continue;
+                }
+                try {
+                    propertyInfo.SetValue(product, sourceValue, BindingFlags.Public, FieldBinder.Instance, null, CultureInfo.GetCultureInfo("Ru-ru"));
+                }
+                catch (Exception ex) {
+                    results.Add((propName, new InvalidOperationException($"Операция не выполнена. Значение \"{sourceValue}\" столбца {propName} не удается привести к типу {propertyInfo.PropertyType}.", ex)));
+                }
+            }
+            return results;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="Tdest"></typeparam>
+        /// <typeparam name="Tprop"></typeparam>
+        /// <param name="product"></param>
+        /// <param name="propName"></param>
+        /// <param name="sourceValue"></param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        public void SetProperty<Tdest, Tprop>(Tdest product, string propName, Tprop sourceValue) where Tdest : class {
+            var productType = product.GetType();
+            var propertyInfo = productType.GetProperty(propName);
+            if (propertyInfo == null) { throw new ArgumentException($"Свойство {propName} не найдено в типе {productType.Name}."); }
+            if (!propertyInfo.CanWrite) {
+                throw new ArgumentException($"Свойство {propName} не в типе {productType.Name} не доступно для записи.");
+            }
+            if (propertyInfo.GetCustomAttribute<KeyAttribute>() != null) {
+                throw new InvalidOperationException($"Операция не разрешена. Свойство {propName} является частью ключа.");
+            }
+            try {
+                propertyInfo.SetValue(product, sourceValue, BindingFlags.Public, FieldBinder.Instance, null, CultureInfo.GetCultureInfo("Ru-ru"));
+            }
+            catch (Exception ex) {
+                throw new InvalidOperationException($"Операция не выполнена. Значение \"{sourceValue}\" свойства {propName} не удается привести к типу {propertyInfo.PropertyType}.", ex);
+            }
+        }
+        private static bool TryConvertToBool(string sourceValue, out bool? result) {
+            if (string.Compare(sourceValue, "True", ignoreCase: true, CultureInfo.InvariantCulture) == 0 ||
+                string.Compare(sourceValue, "Да", ignoreCase: true, CultureInfo.GetCultureInfo("Ru-ru")) == 0
+                ) {
+                result = true;
+                return true;
+            }
+            else if (string.Compare(sourceValue, "False", ignoreCase: true, CultureInfo.InvariantCulture) == 0 ||
+                string.Compare(sourceValue, "Нет", ignoreCase: true, CultureInfo.GetCultureInfo("Ru-ru")) == 0
+                ) {
+                result = false;
+                return true;
+            }
+            else {
+                result = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="Tdest"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static string[] GetProductProperties<Tdest>() where Tdest : class {
+            var type = typeof(Tdest);
+            var typeName = type.Name;
+            if (propertiesCache.TryGetValue(typeName, out var properties)) {
+                return properties;
+            }
+            var contextType = typeof(Context);
+            if (type.Assembly != contextType.Assembly) {
+                throw new InvalidOperationException($"Тип {typeName} не принадлежит сборке, в которой определен класс Nano.Electric.Context.");
+            }
+            var attr = typeof(Tdest).GetCustomAttribute<System.ComponentModel.DataAnnotations.Schema.TableAttribute>();
+            if (attr is null) {
+                throw new InvalidOperationException($"Тип {typeName} не имеет атрибута TableAttribute.");
+            }
+            // out public propertis, exept properties width attribute [NotMapped]
+            properties = type.GetProperties()
+                .Where(p => /*p.GetCustomAttribute<System.ComponentModel.DataAnnotations.Schema.NotMappedAttribute>() == null &&*/ p.CanWrite)
+                .Select(p => p.Name)
+                .ToArray();
+            propertiesCache[typeName] = properties;
+            return properties;
+        }
+        public static string GetDefaultLocalizeValue<T>() where T : class {
+            Type productType = typeof(T);
+            return GetDefaultLocalizeValue(productType);
+        }
+        public static string GetDatabaseTableName(Type productType) {
+            var table = productType.GetCustomAttribute<TableAttribute>()?.Name ?? productType.Name;
+            return table;
+        }
+        public static string GetDefaultLocalizeValue(Type productType) {
+            if (knownLocalizeValues.ContainsKey(productType))
+                return knownLocalizeValues[productType];
+            var value = productType.GetCustomAttribute<DefaultLocalizeValueAttribute>()?.DefaultLocalizeValue ?? string.Empty;
+            knownLocalizeValues.Add(productType, value);
+            return value;
+        }
+        public DbImage CreateImage(string? imgName, string category, byte[] image) {
+            DbImage dbImg = this.DbImages.Create();
+            int id = 1;
+            if (this.DbImages.Local.Any()) {
+                id = Math.Max(id, this.DbImages.Local.Max(img => img.Id) + 1);
+            }
+            if (this.DbImages.Any()) {
+                id = Math.Max(this.DbImages.Max(img => img.Id) + 1, id);
+            }
+            dbImg.Id = id;
+            dbImg.Text = imgName ?? string.Empty;
+            dbImg.Category = category;
+            dbImg.Image = image;
+            //var data = image;
+            this.DbImages.Add(dbImg);
+            return dbImg;
+        }
 #if NETFRAMEWORK
         public Context(DbConnection existingConnection, bool contextOwnsConnection) : base(existingConnection, contextOwnsConnection) {
         }
