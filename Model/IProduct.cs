53ee7daa1e0288d2ae8d2c5c07363dffebe79c33
Mishkin I.N. <Mishkin_Ivan@mail.ru>
2025-08-17 09:48:58 +0400

    Bs.Nano.Electric.Model v1.25.2

--- Model/IProduct.cs
+++ Model/IProduct.cs
@@ -18,12 +18,20 @@ namespace Nano.Electric {
         ScsCableSystemType? CableSystemType { get; set; }
 
     }
+
     public interface IProduct : IHaveId {
         string Code { get; }
         //int Id { get; set; }
         int? DbImageRef { get; set; }
         string Name { get; set; }
         string Manufacturer { get; set; }
+    }
+    public interface ICommonProduct : IProduct { 
+        string? Series { get; set; }
+        string? Description { get; set; }
+        string? SpecDescription { get; set; }
+        string? Url { get; set; }
+
     }
     public partial class SafeDegree : IHaveId { }
     public partial class ClimateTable : IHaveId { }
@@ -34,69 +42,69 @@ namespace Nano.Electric {
     public partial class ElWireConductMaterial : IHaveId { }
     public partial class ElWireIsolationMaterial : IHaveId { }
     public partial class ScsCableSystemType : IHaveId { }
-    public partial class ScsGutterCanal : IProduct, IHaveImageRef { }
-    public partial class ScsCabelCanal : IProduct, IHaveImageRef { }
-    public partial class ScsCableFitting : IProduct, IHaveImageRef { }
-    public partial class ScsPatchCord : IProduct, IHaveCableSystemTypeRef, IHaveImageRef { }
-    public partial class ScsPipe : IProduct, IHaveImageRef { }
-    public partial class ScsTubeFitting : IProduct, IHaveImageRef { }
-    public partial class DbScsGutterCover : IProduct, IHaveImageRef { }
-    public partial class DbScsGutterPartition : IProduct, IHaveImageRef { }
-    public partial class ScsGcFitting : IProduct, IHaveImageRef { }
-    public partial class DbScsGcCoverUnit : IProduct, IHaveImageRef { }
-    public partial class DbScsGcAccessoryUnit : IProduct, IHaveImageRef { }
-    public partial class ScsGutterBolting : IProduct, IHaveImageRef { }
-    public partial class DbScsGcBoltingAccessoryUnit : IProduct, IHaveImageRef { }
-    public partial class DcCableCanalCover : IProduct, IHaveImageRef   { }
-    public partial class DbCableCanalPartition : IProduct, IHaveImageRef   { }
-    public partial class CaeMaterialUtility : IProduct, IHaveImageRef { }
-    public partial class ElBoard : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElBox : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate { }
-    public partial class ElShieldingUnit : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate { }
-    public partial class ElAutomat : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElSafeDevice : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElKnifeSwitch : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElUzo : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class DbElUzdp : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElStarter : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElOvervoltageSuppressor : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElFrequenceTransformer : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElCasing : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElFiderUtility : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate { }
-    public partial class ElBoardUtility : IProduct, IHaveImageRef { }
-    public partial class ElBlock : IProduct, IHaveImageRef, IHaveDbGraphicRef { }
-    public partial class ElPushButtonStation : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElControlDevice : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElCurrentTransformer : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElCounter : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElVoltmeter : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElAmperemeter : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElLighting : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElLamp : IProduct, IHaveImageRef { }
-    public partial class ElLightUtility : IProduct, IHaveImageRef { }
-    public partial class ElDbEngine : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElDbHeater : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElDbComplex : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class DbElSwitch : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class DbElCSwitch : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class DbElSocket : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElDbCase : IProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
-    public partial class ElSocketUtility : IProduct, IHaveImageRef, IHaveSafeDegree, IHaveDbClimate { }
+    public partial class ScsGutterCanal : ICommonProduct, IHaveImageRef { }
+    public partial class ScsCabelCanal : ICommonProduct, IHaveImageRef { }
+    public partial class ScsCableFitting : ICommonProduct, IHaveImageRef { }
+    public partial class ScsPatchCord : ICommonProduct, IHaveCableSystemTypeRef, IHaveImageRef { }
+    public partial class ScsPipe : ICommonProduct, IHaveImageRef { }
+    public partial class ScsTubeFitting : ICommonProduct, IHaveImageRef { }
+    public partial class DbScsGutterCover : ICommonProduct, IHaveImageRef { }
+    public partial class DbScsGutterPartition : ICommonProduct, IHaveImageRef { }
+    public partial class ScsGcFitting : ICommonProduct, IHaveImageRef { }
+    public partial class DbScsGcCoverUnit : ICommonProduct, IHaveImageRef { }
+    public partial class DbScsGcAccessoryUnit : ICommonProduct, IHaveImageRef { }
+    public partial class ScsGutterBolting : ICommonProduct, IHaveImageRef { }
+    public partial class DbScsGcBoltingAccessoryUnit : ICommonProduct, IHaveImageRef { }
+    public partial class DcCableCanalCover : ICommonProduct, IHaveImageRef   { }
+    public partial class DbCableCanalPartition : ICommonProduct, IHaveImageRef   { }
+    public partial class CaeMaterialUtility : ICommonProduct, IHaveImageRef { }
+    public partial class ElBoard : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElBox : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate { }
+    public partial class ElShieldingUnit : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate { }
+    public partial class ElAutomat : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElSafeDevice : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElKnifeSwitch : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElUzo : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class DbElUzdp : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElStarter : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElOvervoltageSuppressor : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElFrequenceTransformer : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElCasing : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElFiderUtility : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate { }
+    public partial class ElBoardUtility : ICommonProduct, IHaveImageRef { }
+    public partial class ElBlock : ICommonProduct, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ElPushButtonStation : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElControlDevice : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElCurrentTransformer : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElCounter : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElVoltmeter : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElAmperemeter : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElLighting : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElLamp : ICommonProduct, IHaveImageRef { }
+    public partial class ElLightUtility : ICommonProduct, IHaveImageRef { }
+    public partial class ElDbEngine : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElDbHeater : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElDbComplex : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class DbElSwitch : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class DbElCSwitch : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class DbElSocket : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElDbCase : ICommonProduct, IHaveImageRef, IHaveExplodeLevel, IHaveSafeDegree, IHaveDbClimate, IHaveDbGraphicRef { }
+    public partial class ElSocketUtility : ICommonProduct, IHaveImageRef, IHaveSafeDegree, IHaveDbClimate { }
     public partial class ElWireMark : IHaveId { }
     public partial class DbGraphic : IHaveId, IDbGraphic { }
 
-    public partial class ScsPanelUtilityUnit : IProduct, IHaveImageRef { }
-    public partial class ScsShellUtility : IProduct, IHaveImageRef { }
-    public partial class ScsSwitchSocketPanel : IProduct, IHaveCableSystemTypeRef, IHaveImageRef, IHaveDbGraphicRef { }
-    public partial class ScsOrganaizerPanel : IProduct, IHaveImageRef, IHaveDbGraphicRef { }
-    public partial class ScsSwitchUtpPanel : IProduct, IHaveCableSystemTypeRef, IHaveImageRef, IHaveDbGraphicRef { }
-    public partial class ScsShellDistr : IProduct, IHaveImageRef, IHaveDbGraphicRef { }
-    public partial class ScsUtpSocket : IProduct, IHaveCableSystemTypeRef, IHaveImageRef { }
-    public partial class ScsCommutatorPanel : IProduct, IHaveImageRef, IHaveDbGraphicRef { }
-    public partial class ScsHatchUtilityUnit : IProduct, IHaveImageRef{ }
-    public partial class ScsServiceColumnUtilityUnit : IProduct, IHaveImageRef{ }
-    public partial class ScsHatch : IProduct, IHaveImageRef, IHaveDbGraphicRef { }
-    public partial class ScsServiceColumn : IProduct, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ScsPanelUtilityUnit : ICommonProduct, IHaveImageRef { }
+    public partial class ScsShellUtility : ICommonProduct, IHaveImageRef { }
+    public partial class ScsSwitchSocketPanel : ICommonProduct, IHaveCableSystemTypeRef, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ScsOrganaizerPanel : ICommonProduct, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ScsSwitchUtpPanel : ICommonProduct, IHaveCableSystemTypeRef, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ScsShellDistr : ICommonProduct, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ScsUtpSocket : ICommonProduct, IHaveCableSystemTypeRef, IHaveImageRef { }
+    public partial class ScsCommutatorPanel : ICommonProduct, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ScsHatchUtilityUnit : ICommonProduct, IHaveImageRef{ }
+    public partial class ScsServiceColumnUtilityUnit : ICommonProduct, IHaveImageRef{ }
+    public partial class ScsHatch : ICommonProduct, IHaveImageRef, IHaveDbGraphicRef { }
+    public partial class ScsServiceColumn : ICommonProduct, IHaveImageRef, IHaveDbGraphicRef { }
     public partial class ScsCord : IProduct, IHaveCableSystemTypeRef, IHaveImageRef { }
     public partial class DbScsTubeSeriesConfiguration : IHaveId { }
     /*
